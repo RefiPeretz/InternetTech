a2cd77bb5c537822a9cbac5596c3cf7e256cb7db
raphael <refi11991@gmail.com>
2016-12-17 18:27:11 +0200

    ready-without comments

--- ex2/main.js
+++ ex2/main.js
@@ -10,6 +10,7 @@ function login_page(){
 	
 	var login_head = document.createElement("h1");
 	var logInBtn = document.createElement("button");
+    logInBtn.setAttribute("id", "logInBtn");
 	login_head.textContent = "Please login\n"
 	var login_input_user = document.createElement("INPUT"); 
 	var login_input_password = document.createElement("INPUT");
@@ -39,23 +40,37 @@ function login_page(){
 
 function check_login_usr_pass(usr,pass){
     if (usr == 'admin' && pass == 'admin') {
-        alert("Sucess");
+        document.body.style.background = "url('back.jpg') no-repeat";
+        document.body.style.backgroundSize = "100%";
+        login_div.style.display = 'none'
+        profile_div.style.display = 'block'
     } else {
 	    alert("username or password incorrect\n Please contact your administrator.");
-        //changeScreens(loginDiv, profileDiv, false);
-        //passwordIn.value = "";
     }
 
+}
+function profile_to_calc(){
+    document.body.style.background = "url('math.jpg') no-repeat";
+    document.body.style.backgroundSize = "100%";
+    profile_div.style.display = 'none'
+    calc_div.style.display = 'block'
 }
 
 function profile_page(){
     profile_div.setAttribute("id", "profile_div");
     var profile_head = document.createElement("h2");
-    profile_head.textContent = "Raphael's Page"
+    profile_head.textContent = "Raphael's Page";
     profile_head.setAttribute("id", "profile_head");
-    profile_div.appendChild(profile_head)
+    profile_div.appendChild(profile_head);
 
-	var table = document.createElement("TABLE")
+    var profileToCalcBtn = document.createElement("button");
+    profileToCalcBtn.setAttribute("id", "profileToCalcBtn");
+    profileToCalcBtn.setAttribute("type", "submit");
+    profileToCalcBtn.setAttribute("onclick", "profile_to_calc()");
+    profileToCalcBtn.textContent = "Calculator";
+    profile_div.appendChild(profileToCalcBtn);
+
+	var table = document.createElement("TABLE");
     table.setAttribute("id", "myTable");
 
     var tblHead = document.createElement("thead");
@@ -129,10 +144,131 @@ function profile_page(){
 
 }
 
+function calc_to_profile(){
+    document.body.style.background = "url('back.jpg') no-repeat";
+    document.body.style.backgroundSize = "100%";
+
+    calc_div.style.display = 'none'
+    profile_div.style.display = 'block'
+
+}
+
+
+function curDisplayCalc(displayToUpdateId, toDisplay) {
+
+    var displayId = "dispInput"+displayToUpdateId.toString();
+    switch(toDisplay) {
+    case "C":
+        document.getElementById(displayId).value = '';
+        break;
+    case "=":
+        document.getElementById(displayId).value = eval(document.getElementById(displayId).value);
+        break;
+    default:
+    	document.getElementById(displayId).value += toDisplay;
+    }
+}
+
+
+var calcSerialNum = 0; //serial number for calculators
+
+function Calc() {
+
+    var calc = document.createElement("div");
+    var num = calcSerialNum++;
+    calc.setAttribute("id", "calc_body");
+    var calcTable = document.createElement("table");
+    calcTable.setAttribute("id", "tableOfCalc");
+
+    //the display row, including its input:
+    var disp = document.createElement("tr");
+    var dispTd = document.createElement("td");
+    dispTd.setAttribute("colspan", "4");
+    var dispInput = document.createElement("input");
+    dispInput.setAttribute("id", "dispInput"+num.toString());
+    dispInput.setAttribute("type", "text");
+    dispTd.appendChild(dispInput);
+    disp.appendChild(dispTd);
+    calcTable.appendChild(disp);
+
+    //this is how the calculator's keyboard will look like:
+    var buttons = ['7', '8', '9', '*',
+        '4', '5', '6', '-',
+        '1', '2', '3', '+',
+        'C', '0', '=', '/',
+		'(',')','.','%'];
+
+    //create the buttons:
+    for (var i=0;i<5;++i) {
+        var row = document.createElement("tr");
+        row.setAttribute("id", "row"+i.toString());
+
+        for (var j=i*4; j<i*4+4; ++j) {
+            var col = document.createElement("td");
+            col.setAttribute("id", (j%4).toString());
+            var btn = document.createElement("button");
+            var buttVal = buttons[j];
+            btn.setAttribute("id", "btnOfCalc");
+            btn.textContent = buttVal;
+            btn.setAttribute("onclick", "curDisplayCalc(\""+num+"\" ,\""+buttVal+"\")");
+            col.appendChild(btn);
+            row.appendChild(col);
+        }
+        calcTable.appendChild(row);
+    }
+    calc.appendChild(calcTable);
+    calc_div.appendChild(calc);
+}
+
+/**
+ * add another calculator to the calculator screen.
+ */
+var addCalc = function() {
+    new Calc();
+}
+
+
+/**
+ * create the components of the calculator screen
+ */
+ function calc_page() {
+    calc_div.setAttribute("id", "calc_div")
+
+    var addCalculator = document.createElement("button");
+    addCalculator.setAttribute("id", "addCalc");
+    addCalculator.textContent = "Add calculator";
+    addCalculator.setAttribute("onclick", "addCalc()");
+
+    var goToProfileBtn = document.createElement("button");
+    goToProfileBtn.setAttribute("id", "goToProfileBtn");
+    goToProfileBtn.textContent = "Profile page";
+    goToProfileBtn.setAttribute("onclick", "calc_to_profile()");
+
+
+    calc_div.appendChild(addCalculator);
+    calc_div.appendChild(document.createElement("br"))
+    calc_div.appendChild(goToProfileBtn);
+    calc_div.appendChild(document.createElement("br"))
+    calc_div.appendChild(document.createElement("br"))
+
+
+
+    new Calc();
+    document.body.appendChild(calc_div);
+
+}
+
 window.onload = function() {
 
     login_page();
     profile_page();
+    calc_page();
+    login_div.style.display = 'block'
+    calc_div.style.display = 'none'
+    profile_div.style.display = 'none'
+    document.body.style.background = "url('welcome.jpg') no-repeat";
+    document.body.style.backgroundSize = "100%";
+
 
 }
    
