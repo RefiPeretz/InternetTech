70588db223b4c81d559b2969ab70dfe0ff481c3f
raphael <refi11991@gmail.com>
2016-12-21 10:56:04 +0200

    comments and submit

--- ex2/main.js
+++ ex2/main.js
@@ -1,10 +1,13 @@
-var login_div = document.createElement("DIV");   
+//Pages divs
+var login_div = document.createElement("DIV");
 var profile_div = document.createElement("DIV");
 var calc_div = document.createElement("DIV");
 var calcCounter = 0;
 
 
-
+/**
+ * create the login page elements and design
+ */
 function login_page(){
 
 	login_div.setAttribute("id", "login_div");
@@ -39,7 +42,10 @@ function login_page(){
 	login_div.appendChild(logInBtn);
 	document.body.appendChild(login_div);
 }
-
+/**
+ * Verify login user name and password if correct transfer
+ * you to the profile page else yields alert
+ */
 function check_login_usr_pass(usr,pass){
     if (usr == 'admin' && pass == 'admin') {
         document.body.style.background = "url('back.jpg') no-repeat";
@@ -51,13 +57,19 @@ function check_login_usr_pass(usr,pass){
     }
 
 }
+/**
+ * Transfer the user from profile page to calc page.
+ */
 function profile_to_calc(){
     document.body.style.background = "url('math.jpg') no-repeat";
     document.body.style.backgroundSize = "100%";
     profile_div.style.display = 'none'
     calc_div.style.display = 'block'
 }
-
+/**
+ * Create the profile page elements and design
+ * like ex1 but in javascript
+ */
 function profile_page(){
     profile_div.setAttribute("id", "profile_div");
     var profile_head = document.createElement("h2");
@@ -146,6 +158,9 @@ function profile_page(){
 
 }
 
+/**
+ * Transfer user form calc page back to profile.
+ */
 function calc_to_profile(){
     document.body.style.background = "url('back.jpg') no-repeat";
     document.body.style.backgroundSize = "100%";
@@ -155,52 +170,61 @@ function calc_to_profile(){
 
 }
 
+/**
+ * Determine the current display of each caclc by
+ * element id.
+ */
+function curDisplayCalc(curId, curCalcScreen) {
 
-function curDisplayCalc(displayToUpdateId, toDisplay) {
-
-    var displayId = "dispInput"+displayToUpdateId.toString();
-    switch(toDisplay) {
+    var displayIdString = "displayInput"+curId.toString();
+    switch(curCalcScreen) {
     case "C":
-        document.getElementById(displayId).value = '';
+        document.getElementById(displayIdString).value = '';
         break;
     case "=":
-        document.getElementById(displayId).value = eval(document.getElementById(displayId).value);
+        document.getElementById(displayIdString).value = eval(document.getElementById(displayIdString).value);
         break;
     default:
-    	document.getElementById(displayId).value += toDisplay;
+    	document.getElementById(displayIdString).value += curCalcScreen;
     }
 }
 
 
 
-
+/**
+ * The constructor of calc. when call define new calculator
+ * and add to the cacl_div(calc page)
+ */
 function Calc() {
 
     var calc_body = document.createElement("div");
-    var num = calcCounter++;
+
     calc_body.setAttribute("id", "calc_body");
     var calcTable = document.createElement("table");
-    calcTable.setAttribute("id", "tableOfCalc");
+    calcTable.setAttribute("id", "calcTable");
 
-    //the display row, including its input:
-    var disp = document.createElement("tr");
-    var dispTd = document.createElement("td");
-    dispTd.setAttribute("colspan", "4");
-    var dispInput = document.createElement("input");
-    dispInput.setAttribute("id", "dispInput"+num.toString());
-    dispInput.setAttribute("type", "text");
-    dispTd.appendChild(dispInput);
-    disp.appendChild(dispTd);
-    calcTable.appendChild(disp);
+    //Create calculator display screen.
+    var curId = calcCounter++;
+    var display = document.createElement("tr");
+    var displayTd = document.createElement("td");
+    //4 buttons is a row.
+    displayTd.setAttribute("colspan", "4");
+    var displayInput = document.createElement("input");
+    displayInput.setAttribute("id", "displayInput"+curId.toString());
+    displayInput.setAttribute("type", "text");
+    displayTd.appendChild(displayInput);
+    display.appendChild(displayTd);
+    calcTable.appendChild(display);
 
-    //this is how the calculator's keyboard will look like:
-    var buttons = ['7', '8', '9', '*',
+    //Determine calc buttons 4 in a row.
+    var calcButtons = ['7', '8', '9', '*',
         '4', '5', '6', '-',
         '1', '2', '3', '+',
         'C', '0', '=', '/',
 		'(',')','.','%'];
 
-    //create the buttons:
+
+    //Create body
     for (var i=0;i<5;++i) {
         var row = document.createElement("tr");
         row.setAttribute("id", "row"+i.toString());
@@ -209,21 +233,23 @@ function Calc() {
             var col = document.createElement("td");
             col.setAttribute("id", (j%4).toString());
             var btn = document.createElement("button");
-            var buttVal = buttons[j];
+            var buttVal = calcButtons[j];
             btn.setAttribute("id", "btnOfCalc");
             btn.textContent = buttVal;
-            btn.setAttribute("onclick", "curDisplayCalc(\""+num+"\" ,\""+buttVal+"\")");
+            btn.setAttribute("onclick", "curDisplayCalc(\""+curId+"\" ,\""+buttVal+"\")");
             col.appendChild(btn);
             row.appendChild(col);
         }
         calcTable.appendChild(row);
     }
+
     calc_body.appendChild(calcTable);
     calc_div.appendChild(calc_body);
 }
 
 /**
- * add another calculator to the calculator screen.
+ * Adds another calculator to the page inline
+ * activates the calc constructor.
  */
 function addCalc() {
     new Calc();
@@ -231,7 +257,7 @@ function addCalc() {
 
 
 /**
- * create the components of the calculator screen
+ * Create the calc page elements and design
  */
  function calc_page() {
     calc_div.setAttribute("id", "calc_div")
@@ -259,7 +285,10 @@ function addCalc() {
     document.body.appendChild(calc_div);
 
 }
-
+/**
+ * Load all the relevant pages on start in order
+ * and determine first page to show.
+ */
 window.onload = function() {
 
     login_page();
